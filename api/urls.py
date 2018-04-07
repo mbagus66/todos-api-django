40c189522392b56138f89c294fbf904fdc85818c
mbagus66 <m.bagus66@gmail.com>
2018-04-07 16:23:13 +0700

    ToDo API App - Add Function (testing ok but no fix item)

--- api/urls.py
+++ api/urls.py
@@ -4,14 +4,14 @@ from .views import CreateView, DetailsView, UserView, UserDetailsView
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = {
-    url(r'^todos/$', CreateView.as_view(), title="create"),
+    url(r'^todos/$', CreateView.as_view(), name="create"),
     url(r'^todos/(?P<pk>[0-9]+)/$',
-        DetailsView.as_view(), title="details"),
+        DetailsView.as_view(), name="details"),
     url(r'^auth/', include('rest_framework.urls',
                            namespace='rest_framework')),
-    url(r'^users/$', UserView.as_view(), title="users"),
+    url(r'^users/$', UserView.as_view(), name="users"),
     url(r'users/(?P<pk>[0-9]+)/$',
-        UserDetailsView.as_view(), title="user_details"),
+        UserDetailsView.as_view(), name="user_details"),
     url(r'^get-token/', obtain_auth_token),
 }
 
